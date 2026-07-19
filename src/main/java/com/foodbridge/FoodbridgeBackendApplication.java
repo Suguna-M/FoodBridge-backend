2456293abf59459b5d911ac6c691879ad2c01ca7
suguna <suguna2023ai@gmail.com>
2026-07-20 00:56:11 +0530

    Foodbridge backend setup

---
+++ src/main/java/com/foodbridge/FoodbridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodbridgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodbridgeBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/foodbridge/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.foodbridge.repository;
+
+public interface UserRepository {
+}
